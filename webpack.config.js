6c0ac363fc206a7c66bf9eb3695095c699a64d81
Hanan T <hanant@softco.loal>
2017-02-22 08:00:40 +0200

    Added foundation support

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,17 @@
+var webpack = require('webpack');
+
 module.exports = {
-    entry: './app/app.jsx',
+    entry: [
+        'script!jquery/dist/jquery.min.js',
+        'script!foundation-sites/dist/js/foundation.min.js',
+        './app/app.jsx'
+    ],
+    externals: {
+        jquery: 'jQuery'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({ '$':'jquery', 'jQuery':'jquery' })
+    ],
     output: {
         path: __dirname,
         filename: './public/bundle.js'
@@ -22,7 +34,7 @@ module.exports = {
         loaders: [
             { 
                 loader: 'babel-loader',
-                query: { presets: ['react', 'es2015'] },
+                query: { presets: ['react', 'es2015', 'stage-0'] },
                 test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/
             }
